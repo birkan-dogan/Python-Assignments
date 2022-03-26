ec5713bf0102f28bf5fbd930d263d6dc3b47cd98
birkan <b.561birkan@gmail.com>
2022-03-26 12:27:04 +0300

    problem 48 was added

---
+++ self_powers.py
@@ -0,0 +1,10 @@
+# the series, 1 ** 1 + 2 ** 2 + 3 ** 3 + ...+ 10 ** 10 = 10405071317
+# Find the last ten digits of the series, 1 ** 1 + 2 ** 2 + 3 ** 3 + ... + 1000 ** 1000
+
+a = 0
+
+for i in range(1,1000):
+
+    a += i ** i
+
+print(str(a)[-10:])
