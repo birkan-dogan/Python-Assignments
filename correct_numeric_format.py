888077af5939e2d61e254b9820139c38a96001a0
birkan <b.561birkan@gmail.com>
2022-03-30 20:47:36 +0300

    the comment line was updated

--- correct_numeric_format.py
+++ correct_numeric_format.py
@@ -1,10 +1,13 @@
-# take the age of the user using input() and while loop
-# write a program that, 1.)takes the age from the user
-# and check the age if it is correct numeric format
-
+"""
+take the age of the user using input() and while loop
+write a program that, 1.)takes the age from the user
+and check the age if it is correct numeric format
+"""
 
 age = input("enter your age: ")
+
 while age.isdigit() == False:
   print("you entered incorrectly!")
   age = input("enter your age please: ")
+
 print("Great! You entered valid input!")
