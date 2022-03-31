3105482a4f6a1c0932a207ca8dfb451b9e7f51e4
birkan <b.561birkan@gmail.com>
2022-03-31 14:51:17 +0300

    it is really useful to generate a password

---
+++ password_generator.py
@@ -0,0 +1,36 @@
+# password generator
+"""
+we will use random module and its methods for creating a new password.
+
+random.randint(a, b)  --> it returns integer in range[a, b]
+random.shuffle(list_name)  --> it shuffle list x in place, and return None
+
+chr() ---> build-in function, convert ASCII to str
+"""
+
+# we want to make a password and password has 3 upper-case, 3 lower-case, 3 number and 2 special character
+
+import random
+
+uppers = [chr(random.randint(65, 90)) for i in range(3)]  # will have random str between A-Z in a list
+
+lowers = [chr(random.randint(97, 122)) for i in range(3)]  # will have random str between a-z in a list
+
+numbers = [chr(random.randint(48, 57)) for i in range(3)]  # will have random str between 0-9 in a list
+
+chars = chr(random.randint(33, 47)) + chr(random.randint(58, 64))  # will have random str special characters in a str
+
+passw = uppers + lowers + numbers + list(chars)
+
+random.shuffle(passw)
+
+"""
+we can also define a function for shuffling
+
+def shuffling(password):
+  templist = list(password)
+  random.shuffle(templist)
+  return "".join(templist)
+
+"""
+print("".join(passw))
